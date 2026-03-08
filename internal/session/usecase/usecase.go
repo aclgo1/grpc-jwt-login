53d97bf3a74f8085cf418691c270d669b5771168
Marcelo Santos <meusocoro5@gmail.com>
2026-03-08 08:20:20 -0300

    update function refresh tokens

--- internal/session/usecase/usecase.go
+++ internal/session/usecase/usecase.go
@@ -41,64 +41,69 @@ func (s *sessionUC) RefreshToken(ctx context.Context, accessTTK, refreshTTK stri
 
 	err := s.verifyRevogedToken(ctx, session.FormatRefreshTokenRepo(refreshTTK))
 
-	if err == redis.Nil {
-		parsedAccess, err := s.tokenAction.ParseToken(accessTTK)
-		if err != nil && err != session.ErrTokenExpired {
-
-			s.logger.Errorf("RefreshToken.ParseToken: %v", err)
-			return nil, fmt.Errorf("RefreshToken.ParseToken: %v", err)
-
-		}
-
-		parsedRefresh, err := s.tokenAction.ParseToken(refreshTTK)
-		if err != nil {
-			s.logger.Errorf("RefreshToken.ParseToken: %v", err)
-			return nil, fmt.Errorf("RefreshToken.ParseToken: %v", err)
-		}
-
-		claimsAccess, err := s.tokenAction.GetClaims(parsedAccess)
-		if err != nil {
-			s.logger.Errorf("RefreshToken.GetClaims: %v", err)
-			return nil, fmt.Errorf("RefreshToken.GetClaims: %v", err)
-		}
-
-		claimsRefresh, err := s.tokenAction.GetClaims(parsedRefresh)
-		if err != nil {
-			s.logger.Errorf("RefreshToken.GetClaims: %v", err)
-			return nil, fmt.Errorf("RefreshToken.GetClaims: %v", err)
-		}
-
-		expUnixRefresh := claimsRefresh["exp"].(float64)
-		if s.tokenAction.IsExpired(expUnixRefresh) {
-			return nil, session.ErrTokenExpired
-		}
-
-		idAccess, _ := claimsAccess["id"].(string)
-		idRefresh, _ := claimsRefresh["id"].(string)
-		typeRefresh, _ := claimsRefresh["type"].(string)
-
-		if idAccess != idRefresh {
-			return nil, session.ErrMistachTokenID
-		}
-
-		if typeRefresh != session.TypeRefreshTTK {
-			return nil, session.ErrTypeTokenInvalid
-		}
-
-		role, _ := claimsAccess["role"].(string)
-
-		// if err := s.tokenRepo.Set(ctx, accessTTK, session.TtlExpAccessTTK); err != nil {
-		// 	return nil, fmt.Errorf("s.tokenRepo.Set: %w", err)
-		// }
-
-		if err := s.tokenRepo.Set(ctx, session.FormatRefreshTokenRepo(refreshTTK), session.TtlExpRefreshTTK); err != nil {
-			return nil, fmt.Errorf("s.tokenRepo.Set: %w", err)
-		}
-
-		return s.createTokens(ctx, idRefresh, role)
+	if err == nil {
+		return nil, session.ErrTokenRevoged
 	}
 
-	return nil, session.ErrTokenRevoged
+	if err != redis.Nil {
+		return nil, fmt.Errorf("internal server error: %w", err)
+	}
+
+	parsedAccess, err := s.tokenAction.ParseToken(accessTTK)
+	if err != nil && err != session.ErrTokenExpired {
+
+		s.logger.Errorf("RefreshToken.ParseToken: %v", err)
+		return nil, fmt.Errorf("RefreshToken.ParseToken: %v", err)
+
+	}
+
+	parsedRefresh, err := s.tokenAction.ParseToken(refreshTTK)
+	if err != nil {
+		s.logger.Errorf("RefreshToken.ParseToken: %v", err)
+		return nil, fmt.Errorf("RefreshToken.ParseToken: %v", err)
+	}
+
+	claimsAccess, err := s.tokenAction.GetClaims(parsedAccess)
+	if err != nil {
+		s.logger.Errorf("RefreshToken.GetClaims: %v", err)
+		return nil, fmt.Errorf("RefreshToken.GetClaims: %v", err)
+	}
+
+	claimsRefresh, err := s.tokenAction.GetClaims(parsedRefresh)
+	if err != nil {
+		s.logger.Errorf("RefreshToken.GetClaims: %v", err)
+		return nil, fmt.Errorf("RefreshToken.GetClaims: %v", err)
+	}
+
+	expUnixRefresh := claimsRefresh["exp"].(float64)
+	if s.tokenAction.IsExpired(expUnixRefresh) {
+		return nil, session.ErrTokenExpired
+	}
+
+	idAccess, _ := claimsAccess["id"].(string)
+	idRefresh, _ := claimsRefresh["id"].(string)
+	typeRefresh, _ := claimsRefresh["type"].(string)
+
+	if idAccess != idRefresh {
+		return nil, session.ErrMistachTokenID
+	}
+
+	if typeRefresh != session.TypeRefreshTTK {
+		return nil, session.ErrTypeTokenInvalid
+	}
+
+	role, _ := claimsAccess["role"].(string)
+
+	// if err := s.tokenRepo.Set(ctx, accessTTK, session.TtlExpAccessTTK); err != nil {
+	// 	return nil, fmt.Errorf("s.tokenRepo.Set: %w", err)
+	// }
+
+	if err := s.tokenRepo.Set(ctx, session.FormatRefreshTokenRepo(refreshTTK), session.TtlExpRefreshTTK); err != nil {
+		return nil, fmt.Errorf("s.tokenRepo.Set: %w", err)
+	}
+
+	return s.createTokens(ctx, idRefresh, role)
+
 }
 
 func (s *sessionUC) ValidToken(ctx context.Context, ttkString string) (jwt.MapClaims, error) {
@@ -140,37 +145,39 @@ func (s *sessionUC) ValidToken(ctx context.Context, ttkString string) (jwt.MapCl
 func (s *sessionUC) GetClaimsRefreshToken(ctx context.Context, ttkString string) (jwt.MapClaims, error) {
 
 	err := s.verifyRevogedToken(ctx, session.FormatRefreshTokenRepo(ttkString))
-	if err == redis.Nil {
-		parsedRefresh, err := s.tokenAction.ParseToken(ttkString)
-		if err != nil {
-			s.logger.Error(session.ErrInvalidToken)
-			return nil, session.ErrInvalidToken
-		}
-
-		claimsRefresh, err := s.tokenAction.GetClaims(parsedRefresh)
-		if err != nil {
-			s.logger.Error(session.ErrInvalidToken)
-			return nil, session.ErrInvalidToken
-		}
-
-		expUnix := claimsRefresh["exp"].(float64)
-		if s.tokenAction.IsExpired(expUnix) {
-			s.logger.Error(session.ErrTokenExpired)
-			return nil, session.ErrTokenExpired
-		}
-
-		if claimsRefresh["type"].(string) != string(session.TypeRefreshTTK) {
-			return nil, session.ErrTypeTokenInvalid
-		}
-
-		return claimsRefresh, nil
-	}
 
 	if err == nil {
 		return nil, session.ErrTokenRevoged
 	}
 
-	return nil, err
+	if err != redis.Nil {
+		return nil, err
+	}
+
+	parsedRefresh, err := s.tokenAction.ParseToken(ttkString)
+	if err != nil {
+		s.logger.Error(session.ErrInvalidToken)
+		return nil, session.ErrInvalidToken
+	}
+
+	claimsRefresh, err := s.tokenAction.GetClaims(parsedRefresh)
+	if err != nil {
+		s.logger.Error(session.ErrInvalidToken)
+		return nil, session.ErrInvalidToken
+	}
+
+	expUnix := claimsRefresh["exp"].(float64)
+	if s.tokenAction.IsExpired(expUnix) {
+		s.logger.Error(session.ErrTokenExpired)
+		return nil, session.ErrTokenExpired
+	}
+
+	if claimsRefresh["type"].(string) != string(session.TypeRefreshTTK) {
+		return nil, session.ErrTypeTokenInvalid
+	}
+
+	return claimsRefresh, nil
+
 }
 
 func (s *sessionUC) RevogeToken(ctx context.Context, ttkAccess, ttkRefresh string) error {
